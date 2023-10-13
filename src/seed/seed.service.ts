98393d70dd4f1436bf1e984eca4af26620ff1065
Hazael Lopez <elhazael134@gmail.com>
2023-10-13 11:46:46 -0600

    Aun no logro la tarea

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,25 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { PokemonService } from 'src/pokemon/pokemon.service';
 
 @Injectable()
 export class SeedService {
 
   private readonly axios: AxiosInstance = axios;
+  private pokemonService: PokemonService;
   
   async executeSeed() {
     try {
       const {data} = await this.axios.get<PokeResponse>(`https://pokeapi.co/api/v2/pokemon?limit=10`);
-      data.results.forEach(({name, url}) => {
+
+      const dtoData = data.results.map(({name, url}) => {
         const segments = url.split('/');
         const no: number = +segments[segments.length - 2];
-
-        console.log({name, no});
+        return {no, name};
       });
-      
-       
+
+      dtoData.forEach(async (data) => {
+        this.pokemonService.create(data);
+      });
+
     } catch (error) {
       console.log(error);
+      throw new BadRequestException(`No data =(`);
     }
     
   }
