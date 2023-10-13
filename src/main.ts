4ad8eb883ca9075ebec92a6ae782fc9234a4db64
Hazael Lopez <elhazael134@gmail.com>
2023-10-12 20:12:48 -0600

    POST - Recibir y validar la data

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Rutas 
   app.setGlobalPrefix('api/v2')
+
+  // Validacion  de nuestros Pipes
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true, 
+    })
+  );
 
   await app.listen(8080);
 }
