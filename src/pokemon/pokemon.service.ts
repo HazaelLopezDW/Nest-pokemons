d391f136328d0987fec87f1538ade1d3074b2cba
Hazael Lopez <elhazael134@gmail.com>
2023-10-12 21:21:26 -0600

    Tarea - Validar valores únicos

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -22,11 +22,7 @@ export class PokemonService {
       return pokemon;
 
     } catch (error) {
-      if(error.code === 11000) {
-        throw new BadRequestException(`This Pokemon exists in DB ${JSON.stringify(error.keyValue)}`);
-      }
-      console.log(error);
-      throw new InternalServerErrorException(`Can't create Pokemon - Check server logs`);
+      this.handleExceptions(error);
     }
    
   }
@@ -63,18 +59,30 @@ export class PokemonService {
 
   async update(term: string, updatePokemonDto: UpdatePokemonDto) {
     const pokemon = await this.findOne(term);
-
     if(updatePokemonDto.name) 
       updatePokemonDto.name = updatePokemonDto.name.toLowerCase();
 
-    await pokemon.updateOne(updatePokemonDto, {new: true});
+    try {
 
+      await pokemon.updateOne(updatePokemonDto, {new: true});
+      return {...pokemon.toJSON(), ...updatePokemonDto};
 
-    return {...pokemon.toJSON(), ...updatePokemonDto};
-
+    } catch (error) {
+      this.handleExceptions(error);
+    }
   }
 
   remove(id: string) {
     return `This action removes a #${id} pokemon`;
+  }
+
+
+  private handleExceptions(error: any) {
+    if(error.code === 11000) {
+      throw new BadRequestException(`This Pokemon exists in DB ${JSON.stringify(error.keyValue)}`);
+    }
+
+    console.log(error);
+    throw new InternalServerErrorException(`Can't create Pokemon - Check server logs`);
   }
 }
